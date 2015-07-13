e9427443a5fd9e11abfd89fe25011c72ea2bf83d
redlavproject <oppvlu@gmail.com>
2015-07-13 09:53:35 +0200

    Update tiger_new.js

--- tigerApp/js/tiger_new.js
+++ tigerApp/js/tiger_new.js
@@ -37,7 +37,7 @@ function initTiger(){
 
        
      var map = L.map('tiger', {
-			     layers: layers // only add one!
+			     layers: aduMap // only add one!
 		    })
 		    .setView([43.8487, 10.5747], 9);
 
