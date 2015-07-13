9b534063f437182ef65f07b0d7bc463430ddee10
redlavproject <oppvlu@gmail.com>
2015-07-13 10:05:23 +0200

    Update tiger_new.js

--- tigerApp/js/tiger_new.js
+++ tigerApp/js/tiger_new.js
@@ -109,7 +109,7 @@ function initTiger(){
     }
     });
     
-    map.on('dblclick', function(e) {
+    map.on('dbclick', function(e) {
     alert(" Coordinate Geografiche Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
    }
      // Restrict to bounds
