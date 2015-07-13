df4ce9da1ea12f7c483985ff714f9ca41073bcd4
redlavapp <oppvlu@gmail.com>
2015-07-13 20:55:29 +0200

    no message

--- tigerApp/js/social.js
+++ tigerApp/js/social.js
@@ -35,16 +35,16 @@ var isMobile = {
     });
 
 $(document).on("click", '.telegram', function() {
-        if( isMobile.any() ) {
+        //if( isMobile.any() ) {
 
             var text = $(this).attr("data-text");
             var url = $(this).attr("data-link");
             var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
-            var whatsapp_url = "tg://msg?text=" + message;
-            window.location.href = whatsapp_url;
-        } else {
-            alert("Please share this article in mobile device");
-        }
+            var telegram_url = "tg://msg?text=" + message;
+            window.location.href = telegram_url;
+        //} else {
+        //    alert("Please share this article in mobile device");
+        //}
 
     });
 	
